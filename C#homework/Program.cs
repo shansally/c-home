efcd33c645602b42e08347df79feb7cdecc06fd4
shansally <sln6124639@gmail.com>
2024-03-22 11:17:13 +0800

    first commit

---
+++ C#homework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
